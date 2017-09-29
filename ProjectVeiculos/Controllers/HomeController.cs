e3b7b22b6916565f3c8bc9596294472d3b491b2a
Leandro Araújo <leandrocaaraujo@gmail.com>
2017-09-29 13:00:20 -0300

    arquivos projeto

---
+++ ProjectVeiculos/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using ProjectVeiculos.Models;
+
+namespace ProjectVeiculos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VeiculosDBEntities db = new VeiculosDBEntities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var veiculos = db.veiculos.Include("tipos_veiculo").ToList();
+            return View(veiculos);
+        }
+
+        public ActionResult Adicionar()
+        {
+            ViewBag.id_tipo_veiculo = new SelectList(db.tipos_veiculo, "id_tipo_veiculo", "nome");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(veiculos veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.veiculos.Add(veiculo);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            ViewBag.id_tipo_veiculo = new SelectList(db.tipos_veiculo, "id_tipo_veiculo", "nome");
+            return View(veiculo);
+        }
+
+        public ActionResult Editar(long id)
+        {
+            veiculos veiculo = db.veiculos.Find(id);
+            ViewBag.id_tipo_veiculo = new SelectList(db.tipos_veiculo, "id_tipo_veiculo", "nome", veiculo.id_tipo_veiculo);
+
+            return View(veiculo);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(veiculos veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(veiculo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            ViewBag.id_tipo_veiculo = new SelectList(db.tipos_veiculo, "id_tipo_veiculo", "nome", veiculo.id_tipo_veiculo);
+
+            return View(veiculo);
+        }
+
+        public ActionResult Excluir(long id)
+        {
+            try
+            {
+                veiculos veiculo = db.veiculos.Find(id);
+                db.veiculos.Remove(veiculo);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            catch
+            {
+                return RedirectToAction("index");
+            }
+        }
+
+        public ActionResult Visualizar(long id)
+        {
+            veiculos veiculo = db.veiculos.Find(id);
+            return View(veiculo);
+        }
+    }
+}
---
+++ ProjectVeiculos/Models/VeiculosMetadado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectVeiculos.Models
+{
+    [MetadataType(typeof(veiculosMetadado))]
+    public partial class veiculos
+    {
+    }
+
+    public class veiculosMetadado
+    {
+        [Required(ErrorMessage="Obrigatório informar a placa")]
+        [StringLength(7, ErrorMessage="A placa deve possuir 7 caracteres")]
+        public string placa { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar o proprietario")]
+        [StringLength(30, ErrorMessage="O proprietario deve possuir no máximo 30 caracteres")]
+        public string proprietario { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o tipo de veiculo")]
+        public int id_tipo_veiculo { get; set; }
+    }
+}
